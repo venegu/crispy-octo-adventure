6491c2181afa7c9c8e6e25f380bb62a19e23e56d
venegu <venegu@gmail.com>
2016-02-12 22:49:35 -0500

    Adding chap 3 parity test

---
+++ 3_functions/parity.js
@@ -0,0 +1,23 @@
+/*****************************************************
+ * Define a recursive function isEven corresponding to
+ * the following description:
+ *   - Zero is even
+ *   - One is odd
+ *   - For any other number N, its evenness is the same
+ *     as N - 2
+ *
+ * The function should accept a number as a param
+ * and return a Boolean.
+ ****************************************************/
+
+function isEven(number) {
+    if (number === 0) {
+        return true;
+    } else if (number === 1) {
+        return false;
+    } else {
+        return isEven(number - 2);
+    }
+}
+
+console.log(isEven(9001));
